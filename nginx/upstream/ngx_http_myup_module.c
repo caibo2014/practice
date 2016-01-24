f73ac51f1182815363a1dd8d0e7783c364d30410
Bo Cai <cai.bo@h3c.com>
2016-01-24 20:27:12 +0800

    upstream module 45%:add a status line parsing
    Signed-off-by: Bo Cai <cai.bo@h3c.com>

--- nginx/upstream/ngx_http_myup_module.c
+++ nginx/upstream/ngx_http_myup_module.c
@@ -9,6 +9,8 @@ static void* ngx_http_myup_create_loc_conf(ngx_conf_t *cf);
 static char* ngx_http_myup_merge_loc_conf(ngx_conf_t *cf, void *prev, void *conf);
 static ngx_int_t myup_process_status_line(ngx_http_request_t *r);
 static ngx_int_t myup_process_header(ngx_http_request_t *r);
+static void myup_upstream_finalize_requset(ngx_http_request_t *r, ngx_int_t rc);
+
 
 typedef struct {
     ngx_http_upstream_conf_t upstream_conf;
@@ -141,9 +143,86 @@ static ngx_int_t myup_process_status_line(ngx_http_request_t *r) {
 		return myup_upstream_process_header(r);
 }
 
+
+static ngx_int_t myup_process_header(ngx_http_request_t *r) {
+    ngx_int_t rc;
+    ngx_table_elt_t *h;
+    ngx_http_upstream_header_t *hh;
+    ngx_http_upstream_main_conf_t *umcf;
+
+    umcf = ngx_http_get_module_main_conf(r, ngx_http_upstream_module);
+    for(;;) {
+        h = ngx_list__push(r->upstream->headers_in.headers);
+        if  (NULL ==  h){
+            return NGX_ERROR;
+        }
+        h->hash = r->header_hash;
+        
+        h->key.len=r->header_name_end - r->header_name_start;
+        h->value.len = r->header_end - header_start;
+        h->key.data=ngx_pnalloc(r->pool,h->key.len+1+h->value.len+1+h->key.len);
+        if(h->key.data == NULL)  {
+            return  NGX_ERROR;
+        }
+        h->value.data =  h->key.data+h->key.len+1;
+        h->lowcase_key = h->key.data + h->key.len+1+h->value.len+1;
+        ngx_memcpy(h->key.data, r->header_name_start, h->key.len);
+        h->key.data[k->ey.len] = '\0';
+        ngx_memcpy(h->value.data, r->header_start, h->value.len);
+        h->value.data[h->value.len] = '\0';
+        
+        if(h->key.len > r->lowcase_index) {
+            ngx_memcpy(h->lowcase_key, r->lowcase_header, h->key.len)
+        } else {
+            ngx_strlow(h->lowcase_key, h->key.data, h->key.len);
+        }
+        
+        hh = ngx_hash_find(&umcf->headers_in_hash, h->hash,
+                            h->lowcase_key, h->key.len);
+        if (hh && hh->handler(r, h, hh->offset)!= NGX_OK) {
+            return  NGX_ERROR;
+        }
+        continue;
+    }
+    if (rc = NGX_HTTP_PARSE_HEADER_DONE) {
+        if(r->upstream->headers_in.server == NULL) {
+            h = ngx_list_push(&r->upstream->headers_in.headers);
+            if(h == NULL){
+                return NGX_ERROR;
+            }
+            h->hash=ngx_hash(ngx_hash(ngx_hash(ngx_hash(ngx_hash('s','e'),'r'),'v'),'e'),'r');
+            ngx_str_set(&h->key, "Server");
+            ngx_str_null(&h->value);
+            h->lowcase_key =(u_char *)"server";
+        }
+        if(r->upstream->headers_in.date == NULL) {
+            h =ngx_list__push(&->upstream->headers_in.headers);
+            if(h ==NULL){
+                return NGX_ERROR;
+            }
+            h->hash= ngx_hash(ngx_hash(ngx_hash('d', 'a'),'t'),'e');
+            ngx_str_set(&h->key,"Date");
+            ngx_str_null(&h->value);
+            h->lowcase_key = (u_char*)"date";
+        }
+        return NGX_OK;
+    }
+
+    if(rc==NGX_AGAIN) {
+        return rc;
+    }
+    ngx_log_error(NGX_LOG_ERR, r->connection->log,0,"error");
+    return NGX_HTTP_UPSTREAM_INVALID_HEADER;
+}
+
+static void myup_upstream_finalize_requset(ngx_http_request_t *r, ngx_int_t rc){
+    ngx_log_error(NGX_LOG_INFO,r->connection->log,0,"finalize");
+}
+
+
 static ngx_int_t ngx_http_myup_handler(ngx_http_request_t *r) {
     ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "myup handler has been called!");
-    /*
+    
     // create myup context
     ngx_http_myup_ctx *myctx = ngx_http_get_module_ctx(r, ngx_http_myup_module);
     if (NULL == myctx) {
@@ -162,10 +241,12 @@ static ngx_int_t ngx_http_myup_handler(ngx_http_request_t *r) {
     
     // get my config struct
     ngx_http_myup_config_t *mycf = (ngx_http_myup_config_t *)ngx_http_get_module_loc_conf(r, ngx_http_myup_module);
-    */
 
     // let's make a simple upstream conf
-    //
+    ngx_http_upstream_t *u = r->upstream;
+    u->conf = &mycf->upstream;
+
+
     return NGX_ERROR;
 }
 
